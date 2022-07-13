5c990c16545c35b6890e1e6fe3e3526d6dd4cd5c
たくのろじぃ / Takunology <takunology.net@gmail.com>
2022-07-13 15:55:33 +0900

    面積計算アプリの実装

--- SurfaceApp/Program.cs
+++ SurfaceApp/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace SurfaceApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("面積計算アプリ");
+            Console.WriteLine("1:長方形 2:三角形 3:円形");
+            int num = int.Parse(Console.ReadLine());
+            if(num == 1)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(Rectangle(a, b));
+            }
+            else if (num == 2)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int b = int.Parse(Console.ReadLine());
+                Console.WriteLine(Triangle(a, b));  
+            }
+            else
+            {
+                int r = int.Parse(Console.ReadLine());
+                Console.WriteLine(Circle(r));
+            }
+        }
+
+        //長方形、三角形、円形
+        public static int Rectangle(int a, int b)
+        {
+            return a * b;
+        }
+
+        public static float Triangle(int a, int b)
+        {
+            return a * b / 2;
+        }
+
+        public static double Circle(int r)
+        {
+            return r * r * Math.PI;
+        }
+    }
+}
